52ea5fd1e17e1cd055fa889e5cec59881e146b8f
Shiv Singh <shivsingh7150@hotmail.com>
2024-05-17 12:32:42 +0530

    Update footer.blade.php

--- resources/views/frontend/includes/footer.blade.php
+++ resources/views/frontend/includes/footer.blade.php
@@ -248,30 +248,3 @@
 
     }
 </style>
-
-<!--<< All JS Plugins >>-->
-<script src="assets/js/jquery-3.7.1.min.js"></script>
-<!--<< Viewport Js >>-->
-<script src="assets/js/viewport.jquery.js"></script>
-<!--<< Bootstrap Js >>-->
-<script src="assets/js/bootstrap.bundle.min.js"></script>
-<!--<< Nice Select Js >>-->
-<script src="assets/js/jquery.nice-select.min.js"></script>
-<!--<< Waypoints Js >>-->
-<script src="assets/js/jquery.waypoints.js"></script>
-<!--<< Counterup Js >>-->
-<script src="assets/js/jquery.counterup.min.js"></script>
-<!--<< Slick Js >>-->
-<script src="assets/js/slick.min.js"></script>
-<!--<< Slick Animation Js >>-->
-<script src="assets/js/slick-animation.min.js"></script>
-<!--<< MeanMenu Js >>-->
-<script src="assets/js/jquery.meanmenu.min.js"></script>
-<!--<< Magnific Popup Js >>-->
-<script src="assets/js/jquery.magnific-popup.min.js"></script>
-<!--<< Wow Animation Js >>-->
-<script src="assets/js/wow.min.js"></script>
-<!--<< Circle Progress Js >>-->
-<script src="assets/js/circle-progress.js"></script>
-<!--<< Main.js >>-->
-<script src="assets/js/main.js"></script>
